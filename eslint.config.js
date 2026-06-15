bb417a6643ab7f8e9fa2b7f9c260d9caed65af32
tomatorivera <tomatorivera@gmail.com>
2026-06-15 14:33:01 -0300

    chore: instalar y configurar eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+// import prettierConfig from "eslint-config-prettier";
+// import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      // prettierConfig,
     ],
+    plugins: {
+      react,
+      "jsx-a11y": jsxA11y,
+      // prettierPlugin,
+    },
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "jsx-a11y/alt-text": "warn",
+      "prettier/prettier": "off",
     },
   },
-])
+  tseslint.configs.recommended,
+]);
